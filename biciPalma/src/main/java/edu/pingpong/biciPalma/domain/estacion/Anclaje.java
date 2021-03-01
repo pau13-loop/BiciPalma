36c8f8c5e4403619d920840ffed17e16c7fef08a
pau <pllinas@cifpfbmoll.eu>
2021-03-01 21:19:11 +0100

    feat: :sparkles: Implementation Anclaje class

--- biciPalma/src/main/java/edu/pingpong/biciPalma/domain/estacion/Anclaje.java
+++ biciPalma/src/main/java/edu/pingpong/biciPalma/domain/estacion/Anclaje.java
@@ -1,5 +1,7 @@
 package edu.pingpong.biciPalma.domain.estacion;
 
+import edu.pingpong.biciPalma.domain.bicicleta.Movil;
+
 public class Anclaje {
     
     private boolean ocupado = false;
@@ -12,19 +14,21 @@ public class Anclaje {
     }
 
     int getBici() {
-        return this.bici;
+        return bici.getId();
     }
 
     void anclarBici(Movil bici) {
+        this.ocupado = true;
+        this.bici = bici;
     }
 
     void liberarBici() {
         this.ocupado = false;
+        this.bici = null;
     }
 
     @Override
     public String toString() {
         return "Ocupado: " + Boolean.toString(isOcupado());
     }
-
 }
