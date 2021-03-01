044ad1e0e01b3a6b0ddd20b319e0109d98224fd8
pau <pllinas@cifpfbmoll.eu>
2021-03-01 20:10:16 +0100

    build: :memo: Creation directory structure

---
+++ biciPalma/src/main/java/edu/pingpong/biciPalma/appPalma/BiciPalmaApp.java
@@ -0,0 +1,13 @@
+package edu.pingpong.biciPalma.appPalma;
+
+/**
+ * Hello world!
+ *
+ */
+public class BiciPalmaApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
