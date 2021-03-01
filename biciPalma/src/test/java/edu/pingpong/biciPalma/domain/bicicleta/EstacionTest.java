a3417677ea6275c44569b10fbd38827a391499df
pau <pllinas@cifpfbmoll.eu>
2021-03-01 20:35:29 +0100

    test: :white_check_mark: Creation test cases Estacion class

---
+++ biciPalma/src/test/java/edu/pingpong/biciPalma/domain/bicicleta/EstacionTest.java
@@ -0,0 +1,24 @@
+package edu.pingpong.biciPalma.domain.bicicleta;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EstacionTest {
+
+    private Estacion estacion;
+
+    @Before
+    public void setupEstacion() {
+        estacion = new Estacion(666, "Intermodal", 5);
+    }
+
+    @Test
+    public void toStringTest() {
+
+        String expected = "Id: 666\nDirección: Intermodal\nNúmero de Anclajes: 5";
+
+        assertEquals(expected, estacion.toString());
+    }
+}
