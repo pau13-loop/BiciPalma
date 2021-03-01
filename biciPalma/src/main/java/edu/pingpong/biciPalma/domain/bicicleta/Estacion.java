b66d8b6717946da262253c6cbb31283acb621885
pau <pllinas@cifpfbmoll.eu>
2021-03-01 20:35:45 +0100

    feat: :sparkles: Implementation estacion class

---
+++ biciPalma/src/main/java/edu/pingpong/biciPalma/domain/bicicleta/Estacion.java
@@ -0,0 +1,36 @@
+package edu.pingpong.biciPalma.domain.bicicleta;
+
+public class Estacion {
+    
+    private final int id;
+    private final String direccion;
+    private int numAnclajes;
+
+    public Estacion(int id, String direccion, int numAnclajes) {
+        this.id = id;
+        this.direccion  = direccion;
+        this.numAnclajes = numAnclajes;
+        
+    }
+
+    private int getId() {
+        return this.id;
+    }
+
+    private String getDireccion() {
+        return this.direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + getId() + "\nDirección: " + getDireccion() + "\nNúmero de Anclajes: " + numAnclajes();
+    }
+
+    private int numAnclajes() {
+        return this.numAnclajes;
+    }
+
+    public void consultarEstacion() {
+        System.out.println(toString());
+    }
+}
