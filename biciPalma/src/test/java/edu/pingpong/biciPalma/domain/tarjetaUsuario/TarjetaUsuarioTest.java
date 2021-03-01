55153cc1cfbacbad7dc25f363b03fc01c3a2e6f8
pau <pllinas@cifpfbmoll.eu>
2021-03-01 21:01:28 +0100

    test: :white_check_mark: Creation test cases for tarjetaUsuario class

---
+++ biciPalma/src/test/java/edu/pingpong/biciPalma/domain/tarjetaUsuario/TarjetaUsuarioTest.java
@@ -0,0 +1,33 @@
+package edu.pingpong.biciPalma.domain.tarjetaUsuario;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TarjetaUsuarioTest {
+    
+    private TarjetaUsuario tarjeta;
+
+    @Before
+    public void setupTarjetaUsuario() {
+        tarjeta = new TarjetaUsuario("Nueva Tarjeta", false);
+    }
+
+    @Test
+    public void activaccionTest() {
+        
+        assertFalse(tarjeta.isActivada());
+
+        tarjeta.setActivada(true);
+
+        assertTrue(tarjeta.isActivada());
+    }
+
+    @Test
+    public void toStringTest() {
+        assertEquals("Nueva Tarjeta", tarjeta.toString());
+    }
+}
