dd69a50adafce27d4a17936b34a4cdf8455d1dd3
pau <pllinas@cifpfbmoll.eu>
2021-03-01 21:18:58 +0100

    test: :white_check_mark: Creation test cases for Anclaje class

---
+++ biciPalma/src/test/java/edu/pingpong/biciPalma/domain/estacion/AnclajeTest.java
@@ -0,0 +1,45 @@
+package edu.pingpong.biciPalma.domain.estacion;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.pingpong.biciPalma.domain.bicicleta.Bicicleta;
+import edu.pingpong.biciPalma.domain.bicicleta.Movil;
+
+public class AnclajeTest {
+
+    private Movil bici;
+    private Anclaje anclaje;
+
+    @Before
+    public void setupAnclaje() {
+        bici = new Bicicleta(999);
+        anclaje = new Anclaje();
+    }
+
+    @Test
+    public void getBiciTest() {
+        anclaje.anclarBici(bici);
+        assertEquals(999, anclaje.getBici());
+    }
+
+    @Test
+    public void ocupadoTest() {
+        assertFalse(anclaje.isOcupado());
+
+        anclaje.anclarBici(bici);
+        assertTrue(anclaje.isOcupado());
+
+        anclaje.liberarBici();
+        assertFalse(anclaje.isOcupado());
+    }
+
+    @Test
+    public void toStringTest() {
+        assertEquals("Ocupado: false", anclaje.toString());
+    }
+}
