4ca857ebf1b19fa5dbc69ad16f634f544208f560
pau <pllinas@cifpfbmoll.eu>
2021-03-12 19:33:19 +0100

    docs: :memo: Added new user story

--- biciPalma/src/main/java/edu/pingpong/biciPalma/appPalma/BiciPalmaApp.java
+++ biciPalma/src/main/java/edu/pingpong/biciPalma/appPalma/BiciPalmaApp.java
@@ -1,5 +1,6 @@
 package edu.pingpong.biciPalma.appPalma;
 
+import edu.pingpong.biciPalma.domain.bicicleta.Bicicleta;
 import edu.pingpong.biciPalma.domain.estacion.Estacion;
 
 public class BiciPalmaApp 
@@ -26,5 +27,22 @@ public class BiciPalmaApp
 		System.out.println("anclajesLibres: " + estacion.anclajesLibres() + "\n");
 
 		estacion.consultarAnclajes();
+
+		/**
+		 * caso TEST anclar bicicleta(s) 
+		 */
+
+		System.out.println("\n **** caso TEST anclar bicicleta(s) **** \n");
+
+		int[] bicicletas = { 291, 292, 293, 294 };
+		
+		Bicicleta bicicleta = null;
+		for (int id : bicicletas) {
+			bicicleta = new Bicicleta(id);
+			estacion.anclarBicicleta(bicicleta);
+		}
+
+		System.out.println("anclajes libres tras generar " + bicicletas.length 
+							+ " bicis: " + estacion.anclajesLibres());
 	}
 }
